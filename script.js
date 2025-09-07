725f58199b1afebc843c1a40862cdd583115388c
lolodotzip <56828143+Lolo280374@users.noreply.github.com>
2025-09-07 12:33:35 +0200

    commit main files

---
+++ script.js
@@ -0,0 +1,159 @@
+const words = 'sword shield knight castle dragon dungeon kingdom crown throne lance joust tournament squire armor chainmail gauntlet greaves visor helm banner crest sigil standard pennant moat drawbridge portcullis battlement turret parapet wall rampart siege catapult ballista trebuchet mangonel battering ram archer crossbow bow arrow quiver pike halberd spear mace flail axe longsword broadsword dagger dirk shortsword scabbard hilt pommel bard horse steed destrier charger warhorse stirrup saddle reins spur jousting tilt lance rest pavilion tent herald trumpet horn bardic minstrel lute harp lyre pipe drum chant hymn choir psalm abbey monastery monk nun friar abbot prior novice cloister chapel cathedral basilica shrine relic pilgrimage pilgrim cross crucifix rosary chalice paten host altar incense censer shrine relics relicarium shrinekeeper shrinewarden parchment manuscript scroll vellum codex quill ink blotch seal wax scribe illuminator calligraphy gilded rune sigil grimoire tome lore legend saga epic ballad chronicle annals heraldry blazon tincture argent or gules sable azure vert purpure ermine vair gyron pale fess bend chevron cross saltire chief base canton quarter escutcheon shield supporter mantling motto chivalry honor valor courtesy loyalty fealty oath vassal liege suzerain overlord serf peasant villein cottar yeoman reeve bailiff steward seneschal chamberlain constable marshal warden castellan baron baronet knight errant paladin crusader templar hospitaller teutonic sultan emir caliph vizier mamluk janissary caravan bazaar caravanserai oasis desert scimitar turban khalifa imam madrasa minaret mosque scriptorium cloister refectory dormitory library priory abbess convent nunneries chantry chanter sexton sacristan bishop archbishop patriarch pope cardinal curate priest deacon cantor cleric chaplain chapbook psalter missal breviary gradual antiphonary sermon homily relic shrine reliquary sanctum sanctified holy sacred profane heresy inquisition stake pyre witch wizard sorcerer warlock alchemist apothecary physician plague pestilence famine famine cart gallows stocks pillory dungeon oubliette torture rack iron maiden thumbscrew manacle shackle fetter chain gibbet pyre cauldron potion elixir draught philter charm amulet talisman relic runestone spell incantation curse hex omen prophecy vision dream oracle soothsayer seer diviner augur druid shaman elder mystic hermit sage scholar philosopher astrologer astronomer mathematician geometer almanac bestiary compendium codex treatise disputation disputatio disputant guild craft mason smith blacksmith goldsmith silversmith coppersmith tinsmith tinker farrier cooper wheelwright carpenter joiner bowyer fletcher baker miller brewer vintner tailor weaver dyer fuller tanner cobbler leatherworker mason sculptor illuminator calligrapher scrivener printer apprentice journeyman master guildhall charter tally tallystick ledger account tallyman reeve reeveship moot mootcourt witan witenagemot counsel council privy councilor regent chancellor ambassador envoy herald messenger bard harper jongleur minstrel troubadour trouvere skald gleeman scops saga teller chronicler annalist historiographer annals codex archive library chronicle parchment vellum palimpsest codex gloss commentary scholastic theology philosophy disputation university college hall cloister quadrangle dormitory refectory scriptorium bursar rector provost chancellor bursary lecture disputatio disputant trivium quadrivium rhetoric grammar logic arithmetic geometry music astronomy rhetoric disputant student apprentice novice adept magister doctor scholar bachelor master feast banquet repast supper dinner trencher platter goblet chalice cup horn tankard stein jug amphora flagon pitcher bread ale mead cider wine venison mutton pork beef fowl goose swan peacock pheasant boar stag hare rabbit trout salmon eel carp pottage stew broth gruel porridge turnip leek cabbage onion garlic herb sage thyme rosemary basil mint spice pepper cinnamon nutmeg clove ginger saffron honey sugar almond walnut hazelnut chestnut fig date raisin currant apple pear quince plum cherry berry orchard harvest threshing flail sickle scythe plough oxen yoke cart wain wagon hay bale sheaf granary barn cowshed byre stable sty dovecote sheepfold shepherd goatherd swineherd cowherd drover herdsman reeve bailiff villein serf peasant manor hall hearth thatch wattle daub timber beam plank post stone brick mortar lime quarry mason quarryman carver sculptor gargoyle grotesque boss arch spire nave transept chancel apse cloister choir chapterhouse crypt catacomb ossuary reliquary shrine shrinewarden abbey monastery priory convent cloister cloistergarth chapterhouse infirmary hospice almshouse leperhouse pesthouse lazarhouse hospital apothecary herbal remedy poultice salve balm potion charm talisman amulet ward signet seal sign banner standard ensign gonfalon pennant oriflamme lance pennon badge crest motto escutcheon tabard surcoat gambeson hauberk chainmail cuirass breastplate pauldron spaulder vambrace rerebrace gauntlet sabaton greave poleyn cuisse gorget bevor visor helm greathelm bascinet sallet barbute armet kettlehat cabasset morion burgonet aventail camail coif surcoat jupon brigandine jack jackchain brigand haubergeon chain coif aventail chausses chaussing codpiece chaussing belt baldric swordbelt pouch purse girdle buckle clasp brooch fibula torque torc diadem circlet coronet crown tiara throne dais canopy tapestry arras curtain carpet rushes stool bench chair settle coffer chest armoire cupboard table board platter trencher candlestick lantern sconce torch cresset brazier firepit hearth chimney flue bell belfry tocsin knell toll peal cloister clock dial hourglass sundial astrolabe quadrant sextant staff compass chart map chart parchment vellum codex scroll illuminator scribe scrivener penknife inkwell blotter seal wax stamp signet sign cipher code secret riddle enigma mystery lore myth legend saga epic ballad chanson romance fable morality miracle mystery play pageant masque masque ball revel feast joust tournament melee tiltyard tilting barrier gallery grandstand pavilion tent field herald heraldry crest arms armorial blazon quartering supporters motto scroll mantling coronet crown peer duke marquess earl count viscount baron baronet knight esquire gentleman lady damsel maiden matron dowager widow spinster bride groom page squire chambermaid handmaiden nurse wetnurse midwife healer witchwise cunningfolk herbalist seer oracle soothsayer charmer conjurer enchanter warlock wizard sorcerer alchemist necromancer astrologer diviner geomancer pyromancer hydromancer aeromancer druid shaman bard sage philosopher scholar monk nun hermit anchorite stylite pilgrim wayfarer traveler messenger envoy herald courier rider horseman knight errant paladin crusader templar hospitaller teutonic lancer spearman pikeman halberdier archer crossbowman bowman longbowman yeoman sergeant man at arms retainer retinue guard sentinel watchman patrol constable marshal warden sheriff reeve bailiff steward seneschal castellan governor magistrate alderman burgess guildmaster craftsman merchant trader peddler hawker chapman factor broker moneychanger banker usurer coiner mint mintmaster treasurer exchequer scribe scrivener clerk notary sealbearer chancellor regent viceroy legate nuncio emissary diplomat envoy herald trumpeter drummer piper harpist minstrel jongleur skald troubadour trouvere bard singer chanter chorister psalmist cantor choir cleric priest monk friar abbot bishop archbishop patriarch pope cardinal pontiff papal curia clergy clergyhouse chapterhouse monastery abbey priory convent hermitage sanctuary shrine altar relic reliquary saint martyr confessor virgin apostle disciple prophet evangelist patriarch patriarchate bishopric diocese parish parishioner sexton sacristan acolyte thurifer thurible incense censer candle taper vigil fast feast holyday festival procession litany prayer psalm hymn chant carol mass office breviary missal psalter gradual antiphonary homily sermon pulpit lectern missive letter writ charter decree edict mandate bull rescript proclamation proclamation treatise statute ordinance code law charter compact covenant oath pledge fealty homage suzerainty overlord vassal liege retainer tenant knight squire page steward chamberlain cupbearer butler marshal constable master servant serf villein cottar peasant swineherd shepherd cowherd goatherd drover reeve bailiff'.split(' ');
+const wordsCount = words.length;
+const gameTime = 30 * 1000;
+window.timer = null;
+window.gameStart = null;
+window.pauseTime = 0;
+
+function addClass(el,name) {
+    el.className += ' '+name;
+}
+function removeClass(el,name) {
+    el.className = el.className.replace(name,'');
+}
+
+function randomWord() {
+    const randomIndex = Math.ceil(Math.random() * wordsCount);
+    return words[randomIndex - 1];
+}
+
+function formatWord(word) {
+    return `<div class="word"><span class="letter">${word.split('').join('</span><span class="letter">')}</span></div>`;
+}
+
+function newGame() {
+    document.getElementById('words').innerHTML = '';
+    for (let i = 0; i < 200; i++) {
+        document.getElementById('words').innerHTML += formatWord(randomWord());
+    }
+    addClass(document.querySelector('.word'), 'current');
+    addClass(document.querySelector('.letter'), 'current');
+    document.getElementById('info').innerHTML = (gameTime / 1000) + '';
+    window.timer = null;
+}
+
+function getWpm() {
+    const words = [...document.querySelectorAll('.word')];
+    const lastTypedWord = document.querySelector('.word.current');
+    const lastTypedWordIndex = words.indexOf(lastTypedWord) + 1;
+    const typedWords = words.slice(0, lastTypedWordIndex);
+    const correctWords = typedWords.filter(word => {
+        const letters = [...word.children];
+        const incorrectLetters = letters.filter(letter => letter.className.includes('incorrect'));
+        const correctLetters = letters.filter(letter => letter.className.includes('correct'));
+        return incorrectLetters.length === 0 && correctLetters.length === letters.length;
+    });
+    return correctWords.length / gameTime * 60000;
+}
+
+function gameOver() {
+    clearInterval(window.timer);
+    addClass(document.getElementById('game'), 'over');
+    const result = getWpm();
+    document.getElementById('info').innerHTML = `WPM: ${result}`;
+}
+
+document.getElementById('game').addEventListener('keyup', ev => {
+    const key = ev.key;
+    const currentWord = document.querySelector('.word.current');
+    const currentLetter = document.querySelector('.letter.current');
+    const expected = currentLetter?.innerHTML || ' ';
+    const isLetter = key.length === 1 && key !== ' ';
+    const isSpace = key === ' ';
+    const isBackspace = key === 'Backspace';
+    const isFirstLetter = currentLetter === currentWord.firstChild;
+
+    if (document.querySelector('#game.over')) {
+        return;
+    }
+
+    console.log({key,expected});
+
+    if (!window.timer && isLetter) {
+        window.timer = setInterval(() => {
+            if (!window.gameStart) {
+                window.gameStart = (new Date()).getTime();
+            }
+            const currentTime = (new Date()).getTime();
+            const msPassed = currentTime - window.gameStart;
+            const sPassed = Math.round(msPassed / 1000);
+            const sLeft = Math.round((gameTime / 1000) - sPassed);
+            if (sLeft <= 0) {
+                gameOver();
+                return;
+            }
+            document.getElementById('info').innerHTML = sLeft + '';
+        }, 1000);
+    }
+
+    if (isLetter) {
+        if (currentLetter) {
+            addClass(currentLetter, key === expected ? 'correct' : 'incorrect');
+            removeClass(currentLetter, 'current');
+            if (currentLetter.nextSibling) {
+                addClass(currentLetter.nextSibling, 'current');
+            }
+        } else {
+            const incorrectLetter = document.createElement('span');
+            incorrectLetter.innerHTML = key;
+            incorrectLetter.className = 'letter incorrect extra';
+            currentWord.appendChild(incorrectLetter);
+        }
+    }
+
+    if (isSpace) {
+        if (expected !== ' ') {
+            const lettersToInvalidate = [...document.querySelectorAll('.word.current .letter:not(.correct)')];
+            lettersToInvalidate.forEach(letter => {
+                addClass(letter, 'incorrect');
+            });
+        }
+        removeClass(currentWord, 'current');
+        addClass(currentWord.nextSibling, 'current');
+        if (currentLetter) {
+            removeClass(currentLetter, 'current');
+        }
+        addClass(currentWord.nextSibling.firstChild, 'current');
+    }
+
+    if (isBackspace) {
+        if (currentLetter && isFirstLetter) {
+            removeClass(currentWord, 'current');
+            addClass(currentWord.previousSibling, 'current');
+            removeClass(currentLetter, 'current');
+            addClass(currentWord.previousSibling.lastChild, 'current');
+            removeClass(currentWord.previousSibling.lastChild, 'incorrect');
+            removeClass(currentWord.previousSibling.lastChild, 'correct');
+        }
+        if (currentLetter && !isFirstLetter) {
+            removeClass(currentLetter, 'current');
+            addClass(currentLetter.previousSibling, 'current');
+            removeClass(currentLetter.previousSibling, 'incorrect');
+            removeClass(currentLetter.previousSibling, 'correct');
+        }
+        if (!currentLetter) {
+            addClass(currentWord.lastChild, 'current');
+            removeClass(currentWord.lastChild, 'incorrect');
+            removeClass(currentWord.lastChild, 'correct');
+        }
+    }
+
+    if (currentWord.getBoundingClientRect().top > 250) {
+        const words = document.getElementById('words');
+        const margin = parseInt(words.style.marginTop || '0px');
+        words.style.marginTop = (margin - 35) + 'px';
+    }
+
+    const nextLetter = document.querySelector('.letter.current');
+    const nextWord = document.querySelector('.word.current');
+    const cursor = document.getElementById('cursor');
+    cursor.style.top = (nextLetter || nextWord).getBoundingClientRect().top + 2 + 'px';
+    cursor.style.left = (nextLetter || nextWord).getBoundingClientRect()[nextLetter ? 'left' : 'right'] + 'px';
+});
+
+document.getElementById('newGameBtn').addEventListener('click', () => {
+    gameOver();
+    newGame();
+});
+
+newGame();
